8f085d7d09ac5370df3748705f3f40ee1b1d2e47
unknown <you@example.com>
2023-01-15 21:25:12 +0300

    Add number square or not

---
+++ TASKS/Task001_Sq_or_not/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Clear();
+Console.Write ("Write 1st number: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write ("Write 2nd number: ");
+int m = Convert.ToInt32(Console.ReadLine());
+if (m * m == n)
+Console.WriteLine("Yes");
+else
+Console.WriteLine("No");
