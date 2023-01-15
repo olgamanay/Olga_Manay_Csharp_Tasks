a93bb84647dd297489fb67a64934484a43e388ac
unknown <you@example.com>
2023-01-15 23:17:26 +0300

    Add -n to n

---
+++ TASKS/Task005_-n+n/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Clear();
+Console.Write ("Write number: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int m = -n;
+while (m <= n)
+{
+    Console.Write($"{m} ");
+    m = m + 1; // или можно сделать сокращение m = m++
+}
