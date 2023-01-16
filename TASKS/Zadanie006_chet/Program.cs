53e9f47d99e4c48bd2a8a8a252d6270cc01eaf59
unknown <you@example.com>
2023-01-16 22:57:23 +0300

    Add zadanie 6 Yes or No

---
+++ TASKS/Zadanie006_chet/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Clear();
+Console.Write ("Write number: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+if ((n % 2) > 0)
+    Console.WriteLine("No");
+    else
+    Console.WriteLine("Yes");
