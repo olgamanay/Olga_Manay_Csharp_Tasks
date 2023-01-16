d39e4ef37a8dea92ed9c5b7a17e2ed6ad528ebb4
unknown <you@example.com>
2023-01-16 23:18:55 +0300

    Add zadanie 8

---
+++ TASKS/Zadanie008_vse_chetni/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Clear();
+Console.Write ("Write number: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int m = Convert.ToInt32(Console.ReadLine());
+while (m <= n)
+{
+Console.Write($"{m+2} ");
+}
