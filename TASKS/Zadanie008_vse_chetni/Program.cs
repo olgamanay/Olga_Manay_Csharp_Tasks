e67ab935c9af842ab5963352d28b8ca616a4cc77
unknown <you@example.com>
2023-01-20 19:15:04 +0300

    add task 8

--- TASKS/Zadanie008_vse_chetni/Program.cs
+++ TASKS/Zadanie008_vse_chetni/Program.cs
@@ -1,19 +1,9 @@
 ﻿Console.Clear();
 Console.Write ("Write number: ");
-int n = Convert.ToInt32(Console.ReadLine());
-
-int m = Convert.ToInt32(Console.ReadLine());
-m = n % 2;
-
-int a = Convert.ToInt32(Console.ReadLine());
-// не понимаю как в этой задаче объединить цикл, который будет искать все гласные, и ограничить их в пределе от 2 до мах (n or n-1)
-if ((m = 0) && (a > 1))
+int n = Convert.ToInt32(Console.ReadLine()), count = 0;
+while (count <= n)
 {
-Console.Write($"{a} ");
-a = n - 2;
+  count = count + 2;
+  Console.Write($"(count) ");  
 }
-else
-{
-Console.Write($"{a+1} ");
-a = n - 2;
-}
+
