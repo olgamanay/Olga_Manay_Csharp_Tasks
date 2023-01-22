b3e8c7bb860cd24cda1509d92e24ceb46d792a4d
unknown <you@example.com>
2023-01-22 23:20:04 +0300

    Add 7 and 23

---
+++ TASKS/Task014/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.Clear();
+Console.Write("Write number: ");
+int n = Convert.ToInt32(Console.ReadLine());
+if (n % 7 == 0 && n % 23 == 0)
+    Console.WriteLine ("yes");
+    else
+    Console.WriteLine ("no");
