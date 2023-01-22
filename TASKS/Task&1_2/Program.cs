8b225ca0af21de9c968950fdaaf165fd751fcd35
unknown <you@example.com>
2023-01-22 14:15:39 +0300

    Task ok

---
+++ TASKS/Task&1_2/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Clear();
+int n = new Random().Next(10, 100);
+Console.WriteLine($"Random number: {n}");
+int n1 = n/10;
+int n2 = n%10;
+if (n1 > n2)
+    Console.WriteLine (n1);
+    else
+    Console.WriteLine (n2);
