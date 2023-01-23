1cd66c1082eab457badcab236df328a9d242b797
unknown <you@example.com>
2023-01-23 10:02:31 +0300

    Add 15 task ???

---
+++ TASKS/Zadanie015_Vihodnoy/Program.cs
@@ -0,0 +1,10 @@
+﻿//Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+//6 -> да 7 -> да 1 -> нет
+
+Console.Clear();
+Console.Write("Write weeks number (1-7): ");
+int n = Convert.ToInt32(Console.ReadLine());
+if (n >= 1 && n <= 5)
+   Console.WriteLine("Not holiday");
+   else (n == 6 || n == 7)
+    Console.WriteLine("It is holiday");
