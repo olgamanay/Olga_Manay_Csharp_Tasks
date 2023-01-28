6dbd480c20376096aacdf988b393607849f0f4e0
unknown <you@example.com>
2023-01-28 22:50:24 +0300

    Add 17 task all right

---
+++ TASKS/Task017_xy/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая принимает на вход 
+//координаты точки (Х, У), причем они не равны 0,
+// и выдает номер четверти плоскости, в которой находится
+// эта точка.
+
+Console.Clear();
+Console.Write("Write x: ");
+double x = Convert.ToDouble(Console.ReadLine());
+Console.Write("Write y: ");
+double y = Convert.ToDouble(Console.ReadLine());
+if (x > 0 && y > 0)
+    Console.WriteLine("1");
+else if (x < 0 && y > 0)
+    Console.WriteLine("2");
+else if (x < 0 && y < 0)
+    Console.WriteLine("3");
+else
+    Console.WriteLine("4");
