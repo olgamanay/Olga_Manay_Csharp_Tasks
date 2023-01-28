55b69b21af8f4550577135990267a88b94bdd1c3
unknown <you@example.com>
2023-01-28 23:11:35 +0300

    Add task 0 about

--- TASKS/Task017_xy/Program.cs
+++ TASKS/Task017_xy/Program.cs
@@ -8,6 +8,15 @@ Console.Write("Write x: ");
 double x = Convert.ToDouble(Console.ReadLine());
 Console.Write("Write y: ");
 double y = Convert.ToDouble(Console.ReadLine());
+
+while (x == 0 || y == 0)
+{
+    Console.WriteLine ("Wrong! Write x and y!");
+    Console.Write("Write x: ");
+x = Convert.ToDouble(Console.ReadLine());
+Console.Write("Write y: ");
+y = Convert.ToDouble(Console.ReadLine());
+}
 if (x > 0 && y > 0)
     Console.WriteLine("1");
 else if (x < 0 && y > 0)
