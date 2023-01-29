287bc387a5bce9a5c87eb6183e4d06e6fe5ce04e
unknown <you@example.com>
2023-01-29 21:30:51 +0300

    Task 22 all right

---
+++ TASKS/Task022_Square1toN/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.Clear();
+Console.Write("Write one natural number: ");
+int n = Convert.ToInt32(Console.ReadLine());
+for(int i = 1; i < n + 1; i++)
+   {
+    Console.Write($"{Math.Pow(i, 2)} ");
+   }
