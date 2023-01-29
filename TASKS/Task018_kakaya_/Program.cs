ce5a9f52043524cdb7df4da4e7e126daa674d904
unknown <you@example.com>
2023-01-29 10:23:46 +0300

    Add 18 task well

---
+++ TASKS/Task018_kakaya_/Program.cs
@@ -0,0 +1,17 @@
+﻿// При вводе номера четверти показывать диапазон возможных координат
+
+
+
+
+Console.Clear();
+Console.Write("Write 1 - 4 : ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+if (n == 1)
+    Console.WriteLine("x > 0 and y > 0");
+else if (n == 2)
+    Console.WriteLine("x < 0 and y > 0");
+else if (n == 3)
+    Console.WriteLine("x < 0 and y < 0");
+else
+    Console.WriteLine("x > 0 and y < 0");
