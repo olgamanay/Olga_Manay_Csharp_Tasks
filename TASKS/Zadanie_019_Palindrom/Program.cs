2b9a002309d0569cd706c9b695928fa0c685d58a
unknown <you@example.com>
2023-01-30 13:34:28 +0300

    Add zadanie 19 all right

---
+++ TASKS/Zadanie_019_Palindrom/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Clear();
+Console.Write("Write number(5): ");
+int n = Convert.ToInt32(Console.ReadLine());
+int n1 = n / 10000;
+int n2 = (n / 1000) % 10;
+int n4 = (n % 100) / 10;
+int n5 = n % 10;
+if (n1 == n5 && n2 == n4)
+   Console.WriteLine("yes");
+else
+    Console.WriteLine("no");
