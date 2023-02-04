ab83995f7564a78a3cc7334433769bbf5930d93c
unknown <you@example.com>
2023-02-04 20:59:06 +0300

    Add dop zadanie pro kusti

---
+++ TASKS/Zadanie_dop2/Program.cs
@@ -0,0 +1,22 @@
+﻿// Сбор черники по кругу (4 куста)
+
+int n = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[n];
+for (int i = 0; i < n; i++)
+    array[i] = Convert.ToInt32(Console.ReadLine());
+
+int maxSumma = 0;
+//                 -1, чтобы не выйти за пределы массива
+for (int i = 1; i < array.Length - 1; i++)
+{
+    int sum = array[i - 1] + array[i] + array[i + 1];
+    if (sum > maxSumma)
+        maxSumma = sum;
+}
+if (array[0] + array[1] + array[array.Length - 1] > maxSumma)
+    maxSumma = array[0] + array[1] + array[array.Length - 1];
+
+if (array[array.Length - 1] + array[array.Length - 2] + array[0] > maxSumma)
+    maxSumma = array[array.Length - 1] + array[array.Length - 2] + array[0];
+
+Console.WriteLine(maxSumma);
