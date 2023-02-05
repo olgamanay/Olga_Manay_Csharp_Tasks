e8fe20886ff4eb895b5f0b2774c18798154a6499
unknown <you@example.com>
2023-02-05 12:03:19 +0300

    Add array (string.Join)

---
+++ TASKS/Task_string_char/Program.cs
@@ -0,0 +1,10 @@
+﻿// string(char)
+
+Console.Clear();
+Console.Write("Write amount of massive's elements: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[n];
+for (int i = 0; i < array.Length; i++)
+    array[i] = new Random().Next(-10, 11); // [-10, 10]
+
+Console.WriteLine($"Rezultat: [{string.Join(", ", array)}];");
