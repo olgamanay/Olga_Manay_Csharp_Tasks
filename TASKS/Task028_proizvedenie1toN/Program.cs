4bbb77f090e60047458764dd3823d486e2afc98d
unknown <you@example.com>
2023-02-06 15:30:22 +0300

    Task 28 all right

---
+++ TASKS/Task028_proizvedenie1toN/Program.cs
@@ -0,0 +1,10 @@
+﻿// Произведение чисел от 1 до введенного числа
+
+
+Console.Clear();
+Console.Write("Write number: ");
+int n = Convert.ToInt32(Console.ReadLine()), count = 1;
+for (int i = 2; i <= n; i++)
+   count = count * i;
+
+Console.WriteLine(count);
