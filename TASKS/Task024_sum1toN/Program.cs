e4c0d48416a679c403cc96c354fdc13d216e8fac
unknown <you@example.com>
2023-02-06 14:16:14 +0300

    all right

---
+++ TASKS/Task024_sum1toN/Program.cs
@@ -0,0 +1,8 @@
+﻿// Сумма чисел от 1 до входного числа
+
+
+Console.Clear();
+Console.Write("Write number: ");
+double n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Result sum: {(1 + n) / 2 * n }");
+
