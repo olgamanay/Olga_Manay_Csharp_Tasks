85bd8acd294c64706dac237f6079b97752d40abf
unknown <you@example.com>
2023-02-06 19:07:31 +0300

    All right zadanie 25

---
+++ TASKS/Zadanie025_A_v_stepeni_B/Program.cs
@@ -0,0 +1,10 @@
+﻿// А в степени Б (ввод в ручную)
+
+Console.Clear();
+Console.Write("Write 1st number: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write("Write 2nd number: ");
+int m = Convert.ToInt32(Console.ReadLine()), result = 1;
+for (int i = 1; i <= m; i++)
+    result = result * n; // result *= n;
+Console.WriteLine(result);    
