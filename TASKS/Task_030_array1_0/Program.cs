7b20739482afc91eee83e9c8a40041a16fac5a30
unknown <you@example.com>
2023-02-06 18:20:16 +0300

    All right task 30

---
+++ TASKS/Task_030_array1_0/Program.cs
@@ -0,0 +1,8 @@
+﻿// Массив из 0 и 1
+
+
+Console.Clear();
+int[] array = new int[8];
+for (int i = 0; i < array.Length; i++)
+    array[i] = new Random().Next(0, 2); // [0, 1]
+Console.WriteLine($"Result: [{string.Join(", ", array)}]");
