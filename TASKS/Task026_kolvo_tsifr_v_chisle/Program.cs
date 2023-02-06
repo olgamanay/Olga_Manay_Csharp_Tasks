557a72013c974c650d946e247e42acf6cd416b5d
unknown <you@example.com>
2023-02-06 15:16:05 +0300

    all right task 26

---
+++ TASKS/Task026_kolvo_tsifr_v_chisle/Program.cs
@@ -0,0 +1,13 @@
+﻿// Количество цифр в числе
+
+
+Console.Clear();
+Console.Write("Write number: ");
+int n = Convert.ToInt32(Console.ReadLine()), count = 0;
+while (n > 0)
+{
+    n = n / 10;
+    count++; // count = count + 1
+}
+
+Console.WriteLine(count);
