5929e27ee81b50a55ec4f85c200fc2adc9814a29
unknown <you@example.com>
2023-02-07 22:19:22 +0300

    All right zadanie 31 dop 3

---
+++ TASKS/Zadanie031_dop3/Program.cs
@@ -0,0 +1,28 @@
+﻿void InputArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    array[i] = new Random().Next(-9, 10); // [-9, 9]
+}
+
+void ReleaseArray(int[] array)
+{
+    int summaPositive = 0, summaNegative = 0;
+    foreach (int element in array) // цикл для перебора внутри массива без нумерации
+    {
+        if (element > 0)
+        summaPositive += element;
+        else
+        summaNegative += element;
+
+    }
+    Console.WriteLine($"Positive numbers's sum: {summaPositive}");
+    Console.WriteLine($"Negative numbers's sum: {summaNegative}");
+}
+
+Console.Clear();
+Console.Write("Write array's count: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[n];
+InputArray(array);
+Console.WriteLine($"Begining array: [{string.Join(", ", array)}]");
+ReleaseArray(array);
