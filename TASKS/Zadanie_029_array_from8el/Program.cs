d811b59aca69233bf8564836c56d1f47a169d3bc
unknown <you@example.com>
2023-02-07 20:21:25 +0300

    Random numbers used

--- TASKS/Zadanie_029_array_from8el/Program.cs
+++ TASKS/Zadanie_029_array_from8el/Program.cs
@@ -1,5 +1,5 @@
 ﻿Console.Clear();
 int[] array = new int[8];
 for (int i = 0; i < array.Length; i++)
-    array[i] = new Random().Next();
+    array[i] = new Random().Next(); //не понимаю как ввести 8 числовой массив в ручную (запросить у пользователя)
 Console.WriteLine($"Result: [{string.Join(", ", array)}]");
