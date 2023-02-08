cf1ca8aa741eb51c2326915340b3a23e8caa17a3
unknown <you@example.com>
2023-02-08 14:27:37 +0300

    All right task 32 plus - minus

---
+++ TASKS/Task032_plus_minus/Program.cs
@@ -0,0 +1,20 @@
+﻿void InputArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    array[i] = new Random().Next(-9, 10); // [-9, 9]
+}
+
+void ReleaseArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+        array[i] *= (-1); // array[i] = array[i] * (-1);
+}
+
+Console.Clear();
+Console.Write("Write array's count: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[n];
+InputArray(array);
+Console.WriteLine($"Begining array: [{string.Join(", ", array)}]");
+ReleaseArray(array);
+Console.WriteLine($"Finishing array: [{string.Join(", ", array)}]");
