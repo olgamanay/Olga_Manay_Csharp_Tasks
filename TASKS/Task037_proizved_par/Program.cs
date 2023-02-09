676db7891d7b4f4c01e9e5eb2894bcb06f55096d
unknown <you@example.com>
2023-02-09 17:12:29 +0300

    All right 37 task

---
+++ TASKS/Task037_proizved_par/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.Clear();
+void IntupArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+        array[i] = new Random().Next(-9, 10); // [-9, 9]
+}
+
+void ReleaseArray(int[] array)
+{
+  
+    for (int i = 0; i < array.Length / 2 + array.Length % 2; i++)
+         Console.Write($"{array[i]  * array[array.Length - i - 1]} ");
+
+}
+
+
+Console.Clear();
+Console.Write("Write array's count: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[n];
+IntupArray(array);
+Console.WriteLine($"Begining array: [{string.Join(", ", array)}]");
+ReleaseArray(array);
