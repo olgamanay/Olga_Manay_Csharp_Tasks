ae1c9012da3aa6f5aa2031ad531ce7d4f6677df0
unknown <you@example.com>
2023-02-10 11:41:46 +0300

    All right zadanie 34

---
+++ TASKS/Zadanie034_chet_v_massive/Program.cs
@@ -0,0 +1,26 @@
+﻿void InputArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+        array[i] = new Random().Next(100, 1000);
+}
+
+
+int CountEvenInArray(int[] array)
+{
+    int count = 0;
+    foreach(int element in array)
+    {
+        if (element % 2 == 0)
+            count++;
+    }
+    return count;
+}
+
+
+Console.Clear();
+Console.Write("Write array's count: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[n];
+InputArray(array);
+Console.WriteLine($"Begining array: [{string.Join(", ", array)}]");
+Console.WriteLine($"Result: {CountEvenInArray(array)} ");
