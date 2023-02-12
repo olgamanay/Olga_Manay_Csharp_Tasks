31fab25ef05b7dfcd7b0774e59d1f3cfecc69bb9
unknown <you@example.com>
2023-02-12 14:46:37 +0300

    All right tash 40

---
+++ TASKS/Task040_storoni_preugolnika/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Clear();
+Console.Write("First triangle's side: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Second triangle's side: ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.Write("Third triangle's side: ");
+int c = Convert.ToInt32(Console.ReadLine());
+if (a + b > c && b + c > a && a + c > b)
+    Console.WriteLine("yes");
+else
+    Console.WriteLine("no");
