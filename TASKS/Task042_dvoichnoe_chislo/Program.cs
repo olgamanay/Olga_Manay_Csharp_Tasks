3be5d0395b212254e4692364a3893bda487abf0e
unknown <you@example.com>
2023-02-12 15:58:04 +0300

    All right task 42

---
+++ TASKS/Task042_dvoichnoe_chislo/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Clear();
+Console.Write("Write number: ");
+int n = Convert.ToInt32(Console.ReadLine());
+string result = String.Empty;
+while (n > 0)
+{
+    result = Convert.ToString(n % 2) + result;
+    n = n / 2;
+}
+Console.WriteLine(result);
