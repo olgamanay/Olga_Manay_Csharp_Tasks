da919e64387472f917295096e799d34f9a4c5399
unknown <you@example.com>
2023-02-12 13:29:12 +0300

    All right task 39

---
+++ TASKS/Task039_perevernut_massive/Program.cs
@@ -0,0 +1,24 @@
+﻿void InputArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+        array[i] = new Random().Next(1, 11);
+}
+
+
+void ReplaceElement(int[] array)
+{
+    for (int i = 0; i < array.Length / 2; i++)
+    {
+        int temp = array[i];
+        array[i] = array[array.Length - i - 1];
+        array[array.Length - i - 1] = temp;
+    }
+}
+Console.Clear();
+Console.Write("Write count's numbers of massive: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[n];
+InputArray(array);
+Console.WriteLine($"Beginning massive: [{string.Join(", ", array)}]");
+ReplaceElement(array);
+Console.WriteLine($"Finishing massive: [{string.Join(", ", array)}]");
