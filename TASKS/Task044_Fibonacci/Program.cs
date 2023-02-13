ec204fec5d927b1c4b6db2c6a567aa6e20610e14
unknown <you@example.com>
2023-02-13 13:22:57 +0300

    All right task 44

---
+++ TASKS/Task044_Fibonacci/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Clear();
+Console.Write("Write number: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int a0 = 0, a1 = 1, x;
+for (int i = 0; i < n; i++)
+{
+    Console.Write($"{a0} ");
+    x = a0 + a1;
+    a0 = a1;
+    a1 = x;
+}
