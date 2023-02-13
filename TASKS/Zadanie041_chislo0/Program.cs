ca4dc872629854a8e05d0ed41961019d9cd76e9b
unknown <you@example.com>
2023-02-13 22:55:43 +0300

    All right zadanie 41

---
+++ TASKS/Zadanie041_chislo0/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Clear();
+Console.Write("Write number: ");
+int n = Convert.ToInt32(Console.ReadLine()), count = 0;
+for (int i = 0; i < n; i++)
+{
+    Console.Write("Write number: ");
+    int x = Convert.ToInt32(Console.ReadLine());
+    if (x > 0)
+        count++;   
+}
+Console.Write($"Result: {count}");
+
