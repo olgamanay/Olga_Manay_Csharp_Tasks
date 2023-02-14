1e2298ffe95f9c70935937a858fd4f9ddf435d13
unknown <you@example.com>
2023-02-14 16:04:30 +0300

    All right 2merniy massive

---
+++ TASKS/Task_2merniy_massive/Program.cs
@@ -0,0 +1,19 @@
+﻿void InputMatrix(int[,] matrix)
+{
+    for(int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+            {
+                matrix[i, j] = new Random().Next(1, 11);
+                Console.Write($"{matrix[i, j]} ");
+            }
+    }
+    Console.WriteLine();
+}
+
+
+Console.Clear();
+Console.Write("Write massive's size: ");
+int[] size = Console.ReadLine().Split().Select(x => int.Parse(x)).ToArray();
+int[,] matrix = new int[size[0], size[1]];
+InputMatrix(matrix);
