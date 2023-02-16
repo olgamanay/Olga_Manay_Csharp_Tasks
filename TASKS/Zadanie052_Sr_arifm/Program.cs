c2068d097b449cc257dea4b041b4a2b5c742fbc5
unknown <you@example.com>
2023-02-16 14:04:21 +0300

    All right zadanie 52

---
+++ TASKS/Zadanie052_Sr_arifm/Program.cs
@@ -0,0 +1,34 @@
+﻿void InputMatrix(double[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = Math.Round(new Random().NextDouble() * (20 + 20) - 20, 2);
+            Console.Write($"{matrix[i, j]} \t");
+        }
+        Console.WriteLine();
+    }
+}
+
+
+void ReleaseMatrix(double[,] matrix)
+{
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        double sum = 0;
+        for (int i = 0; i < matrix.GetLength(0); i++)
+        {
+            sum += matrix[i, j];
+        }
+        Console.WriteLine($"Result sr arifm: {j + 1} = {sum / matrix.GetLength(0)}");
+    }
+}
+
+
+Console.Clear();
+Console.Write("Write massive's size: ");
+int[] size = Console.ReadLine().Split().Select(x => int.Parse(x)).ToArray();
+double[,] matrix = new double[size[0], size[1]];
+InputMatrix(matrix);
+ReleaseMatrix(matrix);
