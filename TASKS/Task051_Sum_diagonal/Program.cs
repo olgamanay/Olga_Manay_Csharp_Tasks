18ec415a1825a34fdab20e8eba5123e554b60358
unknown <you@example.com>
2023-02-16 11:01:04 +0300

    All right task 51

---
+++ TASKS/Task051_Sum_diagonal/Program.cs
@@ -0,0 +1,48 @@
+﻿void InputMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = new Random().Next(1, 11); // [1, 10]
+        }
+    }
+}
+
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]} \t");
+        }
+        Console.WriteLine();
+    }
+}
+
+
+int ReleaseMatrix(int[,] matrix)
+{
+    int summa = 0;
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (i == j)
+                summa += matrix[i, j];
+        }
+    }
+    return summa;
+}
+
+
+Console.Clear();
+Console.Write("Write massive's size: ");
+int[] size = Console.ReadLine().Split().Select(x => int.Parse(x)).ToArray();
+int[,] matrix = new int[size[0], size[1]];
+InputMatrix(matrix);
+Console.WriteLine("Beginning massive:");
+PrintMatrix(matrix);
+Console.WriteLine($"Finishing massive: {ReleaseMatrix(matrix)}");
