b0434d8d4a653c3e281ccd120a2a4f7b45fae143
unknown <you@example.com>
2023-02-17 22:46:55 +0300

    All right task 59

---
+++ TASKS/Task059_naimen_el_mas/Program.cs
@@ -0,0 +1,64 @@
+﻿void FillMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = new Random().Next(1, 11);
+        }       
+    }
+}
+
+void ScreenMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(1); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]} \t");
+        }
+        Console.WriteLine();
+    }
+}
+
+void ReleaseMatrix(int[,] matrix)
+{
+    int min = matrix[0, 0];
+    int minIndexI = 0;
+    int minIndexJ = 0;
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (matrix[i, j] < min)
+            {
+            min = matrix[i, j];
+            minIndexI = i;
+            minIndexJ = j;
+            }
+        }
+    }
+
+Console.WriteLine($"min = {min}; [{minIndexI}], [{minIndexJ}]");
+
+for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        if (i != minIndexI)
+        {
+            for (int j = 0; j < matrix.GetLength(1); j++)
+            {
+                if (j != minIndexJ)
+                    Console.Write($"{matrix[i, j]} \t");
+            } 
+            Console.WriteLine();       
+        }
+    } 
+}
+Console.Clear();
+Console.Write("Write massive's size: ");
+int[] size = Console.ReadLine().Split().Select(x => int.Parse(x)).ToArray();
+int[,] matrix = new int[size[0], size[1]];
+FillMatrix(matrix);
+ScreenMatrix(matrix);
+Console.WriteLine();
+ReleaseMatrix(matrix);
