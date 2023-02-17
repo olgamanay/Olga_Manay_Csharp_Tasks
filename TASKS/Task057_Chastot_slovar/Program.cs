ff118d5005f80c1e61beb594e0b3c6f7f0115310
unknown <you@example.com>
2023-02-17 21:26:55 +0300

    All right task057

---
+++ TASKS/Task057_Chastot_slovar/Program.cs
@@ -0,0 +1,67 @@
+﻿bool checkElement(int[] array, int number)
+{
+    foreach (int element in array)
+    {
+        if (number == element)
+            return false;
+    }
+    return true;
+}
+
+int FillMatrix(int[,] matrix, int[] HelpArray)
+{
+    int k = 0;
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            int number = new Random().Next(1, 11);
+            matrix[i, j] = number;
+            if (checkElement(HelpArray, number) == true)
+            {
+                HelpArray[k] = number;
+                k++;
+            }
+        }       
+    }
+    return k;
+}
+
+void ScreenMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(1); i++)
+    {
+        for(int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]} \t");
+        }
+        Console.WriteLine();
+    }
+}
+
+void ReleaseMatrix(int[,] matrix, int[] array, int count)
+{
+    for (int k = 0; k < count; k++)
+    {
+        int countUnique = 0;
+        for (int i = 0; i < matrix.GetLength(1); i++)
+          {
+              for(int j = 0; j < matrix.GetLength(1); j++)
+              {
+                 if (array[k] == matrix[i, j])
+                     countUnique++;
+              }
+          } 
+          Console.WriteLine($"Number {array[k]} is {countUnique} raz");
+    }
+}
+
+Console.Clear();
+Console.Write("Write massive's size: ");
+int[] size = Console.ReadLine().Split().Select(x => int.Parse(x)).ToArray();
+int[,] matrix = new int[size[0], size[1]];
+int[] HelpArray = new int[size[0] * size[1]];
+int k = FillMatrix(matrix, HelpArray);
+ScreenMatrix(matrix);
+Console.WriteLine();
+ReleaseMatrix(matrix, HelpArray, k);
