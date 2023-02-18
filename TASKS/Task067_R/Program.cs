c26c98597f10ed739c5eb6e344a35a9824635088
unknown <you@example.com>
2023-02-18 16:06:55 +0300

    All right task 67

---
+++ TASKS/Task067_R/Program.cs
@@ -0,0 +1,14 @@
+﻿// Прием числа и выдача суммы его цифр
+
+
+int rec(int a)
+{
+    if (a / 10 == 0 || a < 10 || a % 10 == a)
+        return a;
+    return rec(a / 10) + a % 10;
+}
+
+
+Console.Clear();
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(rec(n));
