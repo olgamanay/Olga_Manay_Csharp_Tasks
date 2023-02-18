f58cf4c99a612034e008e02e687ef6cc25fa909b
unknown <you@example.com>
2023-02-18 11:50:54 +0300

    All right zadanie diagonals

---
+++ TASKS/Zadanie_Diagonals/Program.cs
@@ -0,0 +1,30 @@
+﻿void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+            Console.Write($"{matrix[i, j]} \t");
+        Console.WriteLine();
+    }
+}
+
+
+Console.Clear();
+Console.Write("Write matrix's size: ");
+int[] size = Console.ReadLine().Split(" ").Select(x => int.Parse(x)).ToArray();
+int[,] matrix = new int[size[0], size[1]];
+int number = 1;
+for (int i = 1; i < matrix.GetLength(0) + matrix.GetLength(1) + 1; i++)
+{
+    for (int j = 0; j < matrix.GetLength(0); j++)
+    {
+        for (int k = 0; k < matrix.GetLength(1); k++)
+        {
+            if (j + k + 1 == i)
+            {
+                matrix[j, k] = number++;
+            }
+        }
+    }
+}
+PrintMatrix(matrix);
