82279781a036e60d5e115117a2ddabb5e0f754b7
unknown <you@example.com>
2023-02-18 18:13:40 +0300

    All right Fibonachi

---
+++ TASKS/Task_Fibonachi/Program.cs
@@ -0,0 +1,15 @@
+﻿int f(int n)
+{
+    if (n == 0)
+        return 0;
+    
+    if (n == 1)
+        return 1;
+    return f(n - 1) + f(n - 2);
+}
+
+
+Console.Clear();
+Console.Write("Write number: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(f(n));
