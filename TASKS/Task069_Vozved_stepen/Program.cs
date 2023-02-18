6c04fe7e8ddd47ce40cdccc18cc40664cec3caf3
unknown <you@example.com>
2023-02-18 16:25:54 +0300

    All right task 69

---
+++ TASKS/Task069_Vozved_stepen/Program.cs
@@ -0,0 +1,16 @@
+﻿// Возвести число a в степень b с помощью рекурсии
+
+int f(int a, int b)
+{
+    if (b == 0)
+        return 1;
+    return f(a, b - 1) * a;
+}
+
+
+Console.Clear();
+Console.Write("Write number: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write("Write power: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(f(n, m));
