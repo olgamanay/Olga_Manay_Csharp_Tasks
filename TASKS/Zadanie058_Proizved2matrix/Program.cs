9819c65e7e0ac21cdb93f639d38901e09bb20526
unknown <you@example.com>
2023-02-18 11:23:31 +0300

    All right zadanie 58

---
+++ TASKS/Zadanie058_Proizved2matrix/Program.cs
@@ -0,0 +1,49 @@
+﻿//Задача 58. Задайте две матрицы. Напишите программу, 
+//которая будет находить произведение двух матриц.
+
+void InputMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+            matrix[i, j] = new Random().Next(1, 11); // [1, 10]
+    }
+}
+
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+            Console.Write($"{matrix[i, j]} \t");
+        Console.WriteLine();
+    }
+}
+
+
+int[,] ReleaseMatrix(int[,] FirstMatrix, int[,] SecondMatrix)
+{
+    int[,] result = new int[FirstMatrix.GetLength(0), FirstMatrix.GetLength(1)];
+    for (int i = 0; i < FirstMatrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < FirstMatrix.GetLength(1); j++)
+            result[i, j] = FirstMatrix[i, j] * SecondMatrix[i, j];
+    }
+    return result;
+}
+
+
+Console.Clear();
+Console.Write("Write matrix's sizes (same): "); // размеры матриц должны быть одинаковые
+int[] size = Console.ReadLine().Split(" ").Select(x => int.Parse(x)).ToArray();
+int[,] FirstMatrix = new int[size[0], size[1]];
+int[,] SecondMatrix = new int[size[0], size[1]];
+InputMatrix(FirstMatrix);
+InputMatrix(SecondMatrix);
+Console.WriteLine("Beginning massive 1: ");
+PrintMatrix(FirstMatrix);
+Console.WriteLine("Beginning massive 2: ");
+PrintMatrix(SecondMatrix);
+Console.WriteLine("Result: ");
+PrintMatrix(ReleaseMatrix(FirstMatrix, SecondMatrix));
