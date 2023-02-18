aceb3dceab8a991eca680b9a89303d77e286ffae
unknown <you@example.com>
2023-02-18 11:17:13 +0300

    All right zadanie 56

---
+++ TASKS/Zadanie056_Pryamougol_massive/Program.cs
@@ -0,0 +1,59 @@
+﻿//Задача 56. Задайте прямоугольный двумерный массив. 
+//Напишите программу, которая будет находить строку
+// с наименьшей суммой элементов.
+
+void InputMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+            matrix[i, j] = new Random().Next(1, 11); // [1, 10]
+    }
+}
+
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+            Console.Write($"{matrix[i, j]} \t");
+        Console.WriteLine();
+    }
+}
+
+
+int ReleaseMatrix(int[,] matrix)
+{
+    int minRow = 0;
+    for (int i = 0; i < matrix.GetLength(1); i++)
+        minRow += matrix[0, i];
+
+    for (int i = 1; i < matrix.GetLength(0); i++)
+    {
+        int sumRow = 0;
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            sumRow += matrix[i, j];
+        }
+        if (minRow > sumRow)
+            minRow = sumRow;
+    }
+    return minRow;
+}
+
+
+Console.Clear();
+Console.Write("Write rectangular 2-massive: ");
+int[] size = Console.ReadLine().Split(" ").Select(x => int.Parse(x)).ToArray();
+while (size[0] == size[1])
+{
+    Console.Write("You are wrong! \nWrite rectangular 2-massive: ");
+    size = Console.ReadLine().Split(" ").Select(x => int.Parse(x)).ToArray();
+}
+int[,] matrix = new int[size[0], size[1]];
+InputMatrix(matrix);
+Console.WriteLine("Beginning massive: ");
+PrintMatrix(matrix);
+Console.WriteLine();
+Console.WriteLine($"Result: {ReleaseMatrix(matrix)}");
