45186ed279cd5b280998381e810794416f69c44f
unknown <you@example.com>
2023-02-18 15:20:04 +0300

    All right task 63

---
+++ TASKS/Zadanie063_R_1toN/Program.cs
@@ -0,0 +1,14 @@
+﻿// 63 - Вывод всех натуральных чисел от 1 до N
+
+string f(int n)
+{
+    if (n == 1)
+        return "1 ";
+    return f(n - 1) + $", {n} ";
+}
+
+
+Console.Clear();
+Console.Write("Write number: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(f(n));
