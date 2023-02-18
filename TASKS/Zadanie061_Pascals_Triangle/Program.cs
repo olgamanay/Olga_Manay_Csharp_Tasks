0633f610f999583fd93e4e3aed6faaae7fe897bb
unknown <you@example.com>
2023-02-18 11:37:01 +0300

    All right zadanie 61

---
+++ TASKS/Zadanie061_Pascals_Triangle/Program.cs
@@ -0,0 +1,35 @@
+﻿// Задача 61(Дополнительная). Вывести первые N строк треугольник Паскаля. 
+//Сделать вывод в виде равнобедренного треугольника.
+
+int factorial(int n)
+{
+    int i, x = 1;
+    for (i = 1; i <= n; i++)
+    {
+        x *= i;
+    }
+    return x;
+}
+
+
+void ReleaseTask(int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j <= (n - i); j++)
+        {
+            Console.Write(" ");
+        }
+        for (int j = 0; j <= i; j++)
+        {
+            Console.Write(" ");
+            Console.Write(factorial(i) / (factorial(j) * factorial(i - j)));
+        }
+        Console.WriteLine();
+    }
+}
+
+
+Console.Write("Write Pascal triangle's rows: ");
+int n = Convert.ToInt32(Console.ReadLine());
+ReleaseTask(n);
