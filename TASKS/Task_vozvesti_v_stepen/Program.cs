9c62ff8507737fca865a2a8c94710dcb6f0967c9
unknown <you@example.com>
2023-02-18 15:03:23 +0300

    All right

---
+++ TASKS/Task_vozvesti_v_stepen/Program.cs
@@ -0,0 +1,12 @@
+﻿// Возвести число a в степень b с помощью рекурсии
+
+int f(int a, int b)
+{
+    if (b == 0)
+        return 1;
+    return f(a, b - 1) * a;
+}
+
+
+Console.Clear();
+Console.WriteLine(f(3, 5));
