4bbfd843d3939529097ed563ac93ed48fae18e6d
unknown <you@example.com>
2023-02-18 15:53:33 +0300

    All right task 65

---
+++ TASKS/Task065_R_sm_Z63/Program.cs
@@ -0,0 +1,14 @@
+﻿string rec(int m, int n)
+{
+    if (m == n)
+        return $"{m} ";
+    return rec(m, n - 1) + $", {n} ";
+}
+
+
+Console.Clear();
+Console.Write("Write 1st number: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write("Write 2ng number: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(rec(n, m));
